48f9d5d1cddbe30edd555fb5bf2728cab3efe0ba
Louis2324 <miguellouis2023@gmail.com>
2025-09-30 16:12:00 +0200

    Added game loop for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include "ChessGame.h"
-using namespace std;
+
 
 int main() {
     ChessGame game;
+    std::string input;
 
-    cout<<"Welcome to KnightMare CLI chess \n";
-    cout<<"Initial Board Setup \n\n\n";
-
+    std::cout<<"Welcome to KnightMare CLI chess \n";
+    std::cout<<"Initial Board Setup \n\n\n";
     game.printBoard();
+    
+    while(true) {
+        std::cout<<"\n \n \n";
+        std::cout<<"Enter your move (e.g,e2e4) or quit: ";
+        std::getline(std::cin,input);
+
+        if(input == "quit" || input == "exit" ) break;
+        Move m;
+        if(!Move::parseAlgebraic(input,m)) {
+            std::cout<<"Invalid move format \n Try something like e2e4 initial file and rank then file and rank you want to go to";
+            continue;
+        }
+        if(!game.makeMove(m)) {
+            std::cout<<"Move Invalid! Try Again. \n";
+            continue;
+        }
+
+        game.printBoard();
+    }
     return 0;
 }
