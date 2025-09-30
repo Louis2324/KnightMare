96134fb6fd126fb4ae060fca298e94309220d162
Louis2324 <miguellouis2023@gmail.com>
2025-09-30 13:17:51 +0200

    Added signatures for move making that including checking bounds and making a move with the move struct and also getting piece at that specified position of the board

--- src/ChessGame.h
+++ src/ChessGame.h
@@ -7,6 +7,9 @@ class ChessGame {
      ChessGame();
      void printBoard() const;
      void initBoard();
+     bool makeMove(const Move& m);
+     bool inBounds(int r , int c) const {return r>=0 && r<8 && c>= 0 && c<8 ; }
+     char getPiece(int r , int c) const {return board[r][c]; }
     private:
      std::vector<std::vector<char>> board;
 };
