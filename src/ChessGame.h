6dd22f7ce3c445e2bafec33df5530cb41ea8c037
Louis2324 <miguellouis2023@gmail.com>
2025-10-01 07:40:36 +0200

    Added new paramater in makeMove

--- src/ChessGame.h
+++ src/ChessGame.h
@@ -10,7 +10,7 @@ class ChessGame {
 
      void printBoard() const;
      void initBoard();
-     bool makeMove(const Move& m);
+     bool makeMove(const Move& m , bool whiteTurn);
      bool inBounds(int r , int c) const {return r>=0 && r<8 && c>= 0 && c<8 ; }
      char getPiece(int r , int c) const {return board[r][c]; }
      void setPiece(int r , int c , char piece);
