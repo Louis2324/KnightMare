33e05a6f1380921a87234323b15aaabe399d01d8
Louis2324 <miguellouis2023@gmail.com>
2025-10-02 08:17:37 +0200

    Changed pawn moves declaration

--- src/PieceMoves.h
+++ src/PieceMoves.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <vector>
 #include "Move.h"
+#include "ChessGame.h"
 
 class PieceMoves {
     public: 
-    static std::vector<Move> getPawnMoves(int r, int c, bool white);
+    static std::vector<Move> getPawnMoves(int r, int c, bool white , const ChessGame& game);
     static std::vector<Move> getRookMoves(int r, int c);
     static std::vector<Move> getKnightMoves(int r, int c);
     static std::vector<Move> getBishopMoves(int r, int c);
